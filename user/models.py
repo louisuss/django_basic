01a244c83f7906cf6b1beadadccccbf963e8b1fa
louis <dobi1115@gmail.com>
2020-06-14 22:45:59 +0900

    Added class UserAdmin in admin.py

--- user/models.py
+++ user/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=64, verbose_name='사용자명')
+    username = models.CharField(max_length=30, verbose_name='사용자명')
     password = models.CharField(max_length=20, verbose_name='비밀번호')
-    registered_dttm = models.DateTimeField(
+    registered_date_time = models.DateTimeField(
         auto_now_add=True, verbose_name='등록시간')
 
     class Meta:
-        db_table = 'user_info'
+        db_table = 'user'
