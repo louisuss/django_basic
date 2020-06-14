9a836117228ea6875bdfba316c7df69d83ca0332
louis <dobi1115@gmail.com>
2020-06-15 00:04:35 +0900

    Add email field

--- user/models.py
+++ user/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=30, verbose_name='사용자명')
+    email = models.EmailField(max_length=254, verbose_name='이메일')
     password = models.CharField(max_length=20, verbose_name='비밀번호')
     registered_date_time = models.DateTimeField(
         auto_now_add=True, verbose_name='등록시간')
