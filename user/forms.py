ad76766a7338fd92b534f4eeaffc3c1d8785240f
louis <dobi1115@gmail.com>
2020-06-15 17:43:30 +0900

    Add errors and make user_id

--- user/forms.py
+++ user/forms.py
@@ -4,8 +4,15 @@ from django.contrib.auth.hashers import check_password
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30, label='사용자 이름')
+    username = forms.CharField(
+        error_messages={
+            'required': '아이디를 입력해주세요.'
+        },
+        max_length=30, label='사용자 이름')
     password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
         max_length=20, widget=forms.PasswordInput, label='비밀번호')
 
     def clean(self):
@@ -17,3 +24,5 @@ class LoginForm(forms.Form):
             user = User.objects.get(username=username)
             if not check_password(password, user.password):
                 self.add_error('password', '비밀번호가 틀렸습니다.')
+            else:
+                self.user_id = user.id
