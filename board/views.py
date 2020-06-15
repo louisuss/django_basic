3b9193f5cf02dcc4d290859b3012ec69c87fafea
louis <dobi1115@gmail.com>
2020-06-15 22:13:45 +0900

    Add paging function

--- board/views.py
+++ board/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from django.http import Http404
 from user.models import User
 from .models import Board
@@ -42,5 +43,12 @@ def board_write(request):
 
 def board_list(request):
     # -id -> 최신 정보순
-    boards = Board.objects.all().order_by('-id')
+    all_boards = Board.objects.all().order_by('-id')
+    # 페이지가 없으면 첫번째 페이지
+    page = int(request.GET.get('p', 1))
+    # 한페이지 두개씩 나오게 설정
+    paginator = Paginator(all_boards, 2)
+    boards = paginator.get_page(page)
+
+    # boards 변수안에 페이지에 대한 모든 정보들이 들어있음
     return render(request, 'board_list.html', {'boards': boards})
